025d56fb126b9f741c6c8121b952f77c54c0a419
yburkul <yburkul123@gmail.com>
2022-05-23 22:13:20 +0530

    Added:Find the leap year or not

---
+++ BasicProgram/BasicProgram/BasicCoreProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    class BasicCoreProgram
+    {
+        public static void LeapYear()
+        {
+            int Year;
+            Console.WriteLine("Enter a Year: ");
+            Year = int.Parse(Console.ReadLine());
+            if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+            {
+                Console.WriteLine("It is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Leap Year");
+            }
+        }
+    }
+}
---
+++ BasicProgram/BasicProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome In Basic Program");
+            int input;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("1: For Leap Year");
+                Console.WriteLine("0: For Exit");
+                input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        BasicCoreProgram.LeapYear();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit\n");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option: ----- Please Enter Vaild Option");
+                        break ;
+                }
+            }
+            while(input != 0); 
+        }
+    }
+}
